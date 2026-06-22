2fc817509d9bf4720479f99d5c89a6a272e350a8
Dandi Azaidane <dandiazaidane05@gmail.com>
2026-06-22 19:58:29 +0700

    first commit

---
+++ counter-app/src/components/DecreaseButton.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// eslint-disable-next-line react/prop-types
+function DecreaseButton({ decrease }) {
+  return (
+    <button className='decrease' onClick={decrease}>
+      <span role="img" aria-label="minus">
+        -
+      </span>
+    </button>
+  );
+}
+
+DecreaseButton.propType = {
+  decrease: PropTypes.func.isRequired,
+};
+
+export default DecreaseButton;
